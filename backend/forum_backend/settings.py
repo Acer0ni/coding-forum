0a3cff8c57c805bb6135914d1398783592e5e0a3
Larry Flint <79536506+LjFlint@users.noreply.github.com>
2021-12-21 18:59:01 -0800

    dockerized front end

--- backend/forum_backend/settings.py
+++ backend/forum_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("FORUM_SECRET_KEY", 'django-insecure-cxlmfcn_ys8oa+3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['api']
+ALLOWED_HOSTS = ['api','localhost']
 
 
 # Application definition
